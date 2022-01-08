abc0fbd33a0ebca1be2f8b1b0a5401636d016ab6
matteo-pellegrino <matteo.pellegrino@buildo.io>
2022-01-08 22:53:48 +0100

    Minor fix

--- components/layout/useTabs.tsx
+++ components/layout/useTabs.tsx
@@ -5,63 +5,64 @@ import { pipe } from "fp-ts/function";
 import { NonEmptyArray } from "fp-ts/NonEmptyArray";
 import { useState } from "react";
 
-type Tab = {
+type TabProps = {
   id: string;
   label: string;
   render: () => JSX.Element;
 };
 
-export const useTabs: (tabs: NonEmptyArray<Tab>) => [JSX.Element, JSX.Element] =
-  (tabs) => {
-    const [ztabs, setTabs] = useState(
-      pipe(tabs, zipper.fromNonEmptyArray, zipper.start)
-    );
-    const tabList = (
-      <Tablist elevation={1}>
-        {pipe(
-          ztabs,
-          zipper.map((tab) => (
-            <Tab
-              key={tab.id}
-              id={tab.id}
-              onSelect={() =>
-                pipe(
-                  ztabs,
-                  zipper.moveByFindFirst((t) => t.id === tab.id),
-                  option.map(setTabs)
-                )
-              }
-              isSelected={tab.id === ztabs.focus.id}
-              aria-controls={`tab-${tab}`}
-            >
-              {tab.label}
-            </Tab>
-          )),
-          zipper.toNonEmptyArray
-        )}
-      </Tablist>
-    );
+export const useTabs: (
+  tabs: NonEmptyArray<TabProps>
+) => [JSX.Element, JSX.Element] = (tabs) => {
+  const [ztabs, setTabs] = useState(
+    pipe(tabs, zipper.fromNonEmptyArray, zipper.start)
+  );
+  const tabList = (
+    <Tablist elevation={1}>
+      {pipe(
+        ztabs,
+        zipper.map((tab) => (
+          <Tab
+            key={tab.id}
+            id={tab.id}
+            onSelect={() =>
+              pipe(
+                ztabs,
+                zipper.moveByFindFirst((t) => t.id === tab.id),
+                option.map(setTabs)
+              )
+            }
+            isSelected={tab.id === ztabs.focus.id}
+            aria-controls={`tab-${tab}`}
+          >
+            {tab.label}
+          </Tab>
+        )),
+        zipper.toNonEmptyArray
+      )}
+    </Tablist>
+  );
 
-    const panels = (
-      <>
-        {pipe(
-          ztabs,
-          zipper.map((tab) => (
-            <Pane
-              key={tab.id}
-              id={`tab-${tab.id}`}
-              role="tabpanel"
-              aria-labelledby={tab.id}
-              aria-hidden={tab.id !== ztabs.focus.id}
-              display={tab.id === ztabs.focus.id ? "block" : "none"}
-            >
-              {tab.render()}
-            </Pane>
-          )),
-          zipper.toNonEmptyArray
-        )}
-      </>
-    );
+  const panels = (
+    <>
+      {pipe(
+        ztabs,
+        zipper.map((tab) => (
+          <Pane
+            key={tab.id}
+            id={`tab-${tab.id}`}
+            role="tabpanel"
+            aria-labelledby={tab.id}
+            aria-hidden={tab.id !== ztabs.focus.id}
+            display={tab.id === ztabs.focus.id ? "block" : "none"}
+          >
+            {tab.render()}
+          </Pane>
+        )),
+        zipper.toNonEmptyArray
+      )}
+    </>
+  );
 
-    return [tabList, panels];
-  };
+  return [tabList, panels];
+};
