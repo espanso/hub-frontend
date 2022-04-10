67747ee9de3746ffb6b6a1da0186f5dac627e008
matteo-pellegrino <matteo.pelle.pellegrino@gmail.com>
2022-04-10 10:47:22 +0200

    Add useResponsive hook. Add Navbar mobile layout.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,13 @@
 import {
+  IconButton,
+  IconButtonAppearance,
   Link,
   majorScale,
+  Menu,
+  MenuIcon,
   Pane,
+  Popover,
+  Position,
   SearchInput,
   Strong,
   TextInputAppearance,
@@ -10,10 +16,17 @@ import { useState } from "react";
 import { Stack } from "./layout";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useResponsive } from "./layout/useResponsive";
+import { pipe } from "fp-ts/function";
+import { array } from "fp-ts";
 
-const NavbarLink = (props: { href: string; children: React.ReactNode }) => (
+const NavbarLink = (props: {
+  href: string;
+  children: React.ReactNode;
+  color?: string;
+}) => (
   <Link href={props.href} display="flex" alignItems="center">
-    <Strong size={400} color="white">
+    <Strong size={400} color={props.color}>
       {props.children}
     </Strong>
   </Link>
@@ -36,6 +49,76 @@ export const Navbar = (props: Props) => {
   };
 
   const [searchValue, setSearchValue] = useState(props.searchInitialValue);
+  const { foldDevices } = useResponsive();
+
+  const searchInput = (
+    <SearchInput
+      // evergreen doesn't infer user defined appearance type
+      appearance={"navbar" as TextInputAppearance}
+      placeholder="Search for wonderful packages!"
+      onKeyDown={onEnter}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+        setSearchValue(e.target.value)
+      }
+      value={searchValue}
+    />
+  );
+
+  const makeLinks = (color?: string) => [
+    <NavbarLink href="/search" color={color}>
+      Explore
+    </NavbarLink>,
+    <NavbarLink
+      href="https://espanso.org/docs/next/packages/creating-a-package/"
+      color={color}
+    >
+      Create a package
+    </NavbarLink>,
+    <NavbarLink href="https://espanso.org/" color={color}>
+      Espanso
+    </NavbarLink>,
+  ];
+
+  const logoDesktop = (
+    <Image
+      height={30}
+      width={172}
+      src="/images/navbar_logo.svg"
+      alt="Espanso Hub"
+      className="clickable"
+      onClick={() => router.push("/")}
+    />
+  );
+
+  const logoMobile = (
+    <Image
+      height={30}
+      width={30}
+      src="/images/navbar_logo_mobile.svg"
+      alt="Espanso Hub"
+      className="clickable"
+      onClick={() => router.push("/")}
+    />
+  );
+
+  const hamburgerMenu = (
+    <Popover
+      position={Position.BOTTOM_LEFT}
+      content={
+        <Menu>
+          <Menu.Group>
+            {pipe(
+              makeLinks(),
+              array.map((l) => <Menu.Item>{l}</Menu.Item>)
+            )}
+          </Menu.Group>
+        </Menu>
+      }
+    >
+      <MenuIcon color="white" />
+    </Popover>
+  );
+
   return (
     <Pane
       display="flex"
@@ -44,35 +127,44 @@ export const Navbar = (props: Props) => {
       height={majorScale(8)}
       justifyContent="center"
     >
-      <Pane display="flex">
-        <Stack units={2} display="flex" flex={1} alignItems="center">
-          <Image
-            height={30}
-            width={172}
-            src="/images/navbar_logo.svg"
-            alt="Espanso Hub"
-            className="clickable"
-            onClick={() => router.push("/")}
-          />
-          <SearchInput
-            // evergreen doesn't infer user defined appearance type
-            appearance={"navbar" as TextInputAppearance}
-            placeholder="Search for wonderful packages!"
-            onKeyDown={onEnter}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setSearchValue(e.target.value)
-            }
-            value={searchValue}
-          />
-        </Stack>
-        <Stack units={4} display="flex" alignContent="center">
-          <NavbarLink href="/search">Explore</NavbarLink>
-          <NavbarLink href="https://espanso.org/docs/next/packages/creating-a-package/">
-            Create a package
-          </NavbarLink>
-          <NavbarLink href="https://espanso.org/">Espanso</NavbarLink>
-        </Stack>
-      </Pane>
+      {foldDevices({
+        mobile: () => (
+          <Stack units={2} display="flex">
+            {logoMobile}
+            {searchInput}
+            <Pane
+              display="flex"
+              flex={1}
+              alignItems="center"
+              justifyContent="center"
+            >
+              {hamburgerMenu}
+            </Pane>
+          </Stack>
+        ),
+        tablet: () => (
+          <Pane display="flex">
+            <Stack units={2} display="flex" flex={1} alignItems="center">
+              {logoMobile}
+              {searchInput}
+            </Stack>
+            <Stack units={4} display="flex" alignContent="center">
+              {makeLinks("white")}
+            </Stack>
+          </Pane>
+        ),
+        desktop: () => (
+          <Pane display="flex">
+            <Stack units={2} display="flex" flex={1} alignItems="center">
+              {logoDesktop}
+              {searchInput}
+            </Stack>
+            <Stack units={4} display="flex" alignContent="center">
+              {makeLinks("white")}
+            </Stack>
+          </Pane>
+        ),
+      })}
     </Pane>
   );
 };
