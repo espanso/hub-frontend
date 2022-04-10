638e319e2a48b675ea9404ff9a030ec27ce2d1a3
matteo-pellegrino <matteo.pelle.pellegrino@gmail.com>
2022-04-10 10:47:20 +0200

    Add Navbar. Fix Stack.

---
+++ pages/search.tsx
@@ -0,0 +1,59 @@
+import { Pane } from "evergreen-ui";
+import { option, either, task, taskEither, array, nonEmptyArray } from "fp-ts";
+import { constant, pipe } from "fp-ts/function";
+import { InferGetStaticPropsType } from "next";
+import { fetchPackagesIndex } from "../api/packagesIndex";
+import { PackagesGrid, Navbar, ContentRow } from "../components";
+import { useRouter } from "next/router";
+import { Package } from "../api/model";
+
+export const getStaticProps = pipe(
+  fetchPackagesIndex,
+  task.map(either.fold(constant(option.none), option.some)),
+  task.map((packagesIndex) => ({
+    props: {
+      packagesIndex,
+    },
+  }))
+);
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Search = (props: Props) => {
+  const router = useRouter();
+  const { q } = router.query;
+  const query = Array.isArray(q) ? q[0] : q;
+
+  const packagesFilter = (p: Package) =>
+    pipe(
+      Object.values(p),
+      array.filter((value) => typeof value === "string"),
+      array.filter((values) =>
+        query !== undefined && query !== ""
+          ? values.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+          : true
+      ),
+      array.isNonEmpty
+    );
+
+  return (
+    <Pane display="flex" flexDirection="column">
+      <ContentRow background="green500">
+        <Navbar searchInitialValue={query} />
+      </ContentRow>
+
+      <ContentRow background="tint2">
+        {pipe(
+          props.packagesIndex,
+          option.map((index) => index.packages),
+          option.map(array.filter(packagesFilter)),
+          option.chain(nonEmptyArray.fromArray),
+          option.map((packages) => <PackagesGrid packages={packages} />),
+          option.toNullable
+        )}
+      </ContentRow>
+    </Pane>
+  );
+};
+
+export default Search;
