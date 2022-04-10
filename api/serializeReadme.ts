0645a84b57716a2c591b867223d2c8e51afdbc28
matteo-pellegrino <matteo.pelle.pellegrino@gmail.com>
2022-04-10 10:47:24 +0200

    Fix literal not self-closed br tag in package md description

--- api/serializeReadme.ts
+++ api/serializeReadme.ts
@@ -4,18 +4,22 @@ import { serialize } from "next-mdx-remote/serialize";
 import remarkGfm from "remark-gfm";
 import { PackageRepo } from "../api/domain";
 
+// Fix for https://github.com/angelorpt/espanso-git/blob/main/README.md?plain=1
+// See https://github.com/espanso/hub-frontend/issues/6
+const fixBreakTag = (html: string) => html.replaceAll("<br>", "<br/>");
+
 export const serializeReadme = taskEither.chain((packageRepo: PackageRepo) =>
   pipe(
     taskEither.tryCatch(
       () =>
-        serialize(packageRepo.readme, {
+        serialize(fixBreakTag(packageRepo.readme), {
           mdxOptions: {
             remarkPlugins: [remarkGfm],
           },
         }),
       (r) =>
         new Error(
-          `${packageRepo.package.id}: Failing to seriliaze readme markdown: ${r}`
+          `${packageRepo.package.id}: Failing to serialize readme markdown: ${r}`
         )
     ),
     taskEither.map((serializedReadme) => ({
