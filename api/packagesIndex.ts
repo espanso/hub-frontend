345733d1284c34735039c019e4d424d756da876f
matteo-pellegrino <matteo.pelle.pellegrino@gmail.com>
2022-04-10 10:47:21 +0200

    Fix package route. Refactor cache to improve flexibility.

--- api/packagesIndex.ts
+++ api/packagesIndex.ts
@@ -1,7 +1,6 @@
 import flatCache from "flat-cache";
 import { either, option, task, taskEither } from "fp-ts";
 import { constant, flow, pipe } from "fp-ts/function";
-import { Task } from "fp-ts/Task";
 import { TaskEither } from "fp-ts/TaskEither";
 import { PackagesIndex } from "./domain";
 import { taskEitherLogError } from "./utils";
@@ -10,15 +9,10 @@ const PACKAGE_INDEX_URL =
   process.env.PACKAGE_INDEX_URL ||
   "https://github.com/espanso/hub/releases/download/v1.0.0/package_index.json";
 
-export const PACKAGE_INDEX_CACHE_ID = "packagesIndex";
+const PACKAGE_INDEX_CACHE_ID = "packagesIndex";
+const CACHE_DIR = process.env.PACKAGE_INDEX_CACHE_DIR || undefined;
 
-const cache = flatCache.load(PACKAGE_INDEX_CACHE_ID);
-
-export const clearPackagesIndexCache: Task<boolean> = task.fromIO(() =>
-  flatCache.clearCacheById(PACKAGE_INDEX_CACHE_ID)
-);
-
-export const fetchPackagesIndex: TaskEither<Error, PackagesIndex> = pipe(
+const fetchPackagesIndexInternal = (cache: flatCache.Cache) =>
   pipe(
     cache.getKey(PACKAGE_INDEX_URL),
     option.fromNullable,
@@ -46,7 +40,11 @@ export const fetchPackagesIndex: TaskEither<Error, PackagesIndex> = pipe(
           })
         ),
       taskEither.of
-    ),
-    taskEitherLogError
-  )
+    )
+  );
+
+export const fetchPackagesIndex: TaskEither<Error, PackagesIndex> = pipe(
+  flatCache.load(PACKAGE_INDEX_CACHE_ID, CACHE_DIR),
+  fetchPackagesIndexInternal,
+  taskEitherLogError
 );
