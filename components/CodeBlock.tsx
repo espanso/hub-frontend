6ddd992206f6ae5bba9746e646d07974eb83cd2a
matteo-pellegrino <matteo.pelle.pellegrino@gmail.com>
2022-04-10 10:47:21 +0200

    Add syntax highlighting

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -10,21 +10,32 @@ import {
   Text,
   TickIcon,
 } from "evergreen-ui";
+import hljs from "highlight.js/lib/core";
+import yaml from "highlight.js/lib/languages/yaml";
+import shell from "highlight.js/lib/languages/shell";
+import plaintext from "highlight.js/lib/languages/plaintext";
+import "highlight.js/styles/atom-one-dark-reasonable.css";
 import { useState } from "react";
+
+hljs.registerLanguage("yaml", yaml);
+hljs.registerLanguage("shell", shell);
+hljs.registerLanguage("plaintext", plaintext);
 
 type Props = {
   content: string;
   showCopyButton?: boolean;
+  syntax?: "yaml" | "shell" | "plaintext";
 };
 
 export const CodeBlock = (props: Props) => {
   const [isCopied, setIsCopied] = useState(false);
+  const language = props.syntax || "plaintext";
+
   return (
     <Card
       display="flex"
       background="default"
       alignItems="center"
-      minHeight={majorScale(5)}
       padding={majorScale(1)}
       elevation={2}
     >
@@ -32,11 +43,14 @@ export const CodeBlock = (props: Props) => {
         flex={1}
         size={400}
         appearance="minimal"
-        color="white"
+        color={language === "yaml" ? "#98c379" : "white"} // hljs yaml does not parse curly brackets
         style={{ whiteSpace: "pre-wrap" }}
-      >
-        {props.content}
-      </Code>
+        dangerouslySetInnerHTML={{
+          __html: hljs.highlight(props.content, {
+            language,
+          }).value,
+        }}
+      />
       {props.showCopyButton && (
         <Popover
           position={Position.TOP_RIGHT}
