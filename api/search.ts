1115f8b8414ae1f0b468d4f861abd40150fd2e59
matteo-pellegrino <matteo.pelle.pellegrino@gmail.com>
2022-04-10 10:47:21 +0200

    Add tags based filtering to search

--- api/search.ts
+++ api/search.ts
@@ -1,13 +1,26 @@
+import { pipe } from "fp-ts/function";
+import { NonEmptyArray } from "fp-ts/NonEmptyArray";
 import Fuse from "fuse.js";
 import { Package } from "./domain";
 
-const options = {
+const textSearchOptions: Fuse.IFuseOptions<Package> = {
   keys: ["name", "author", "description", "title"],
 };
 
-export const search: (
+export const textSearch: (
   packages: Array<Package>
-) => (query: string) => Array<Package> = (packages) => (query) => {
-  const fuse = new Fuse(packages, options);
-  return fuse.search(query).map((i) => i.item);
+) => (query: string) => Array<Package> = (packages) => (query) =>
+  new Fuse(packages, textSearchOptions).search(query).map((i) => i.item);
+
+const tagSearchOptions: Fuse.IFuseOptions<Package> = {
+  useExtendedSearch: true, // enables '|' OR operator
+  threshold: 0.0, // exact match
+  keys: ["tags"],
 };
+
+export const tagsSearch: (
+  packages: Array<Package>
+) => (tags: NonEmptyArray<string>) => Array<Package> = (packages) => (tags) =>
+  new Fuse(packages, tagSearchOptions)
+    .search(tags.join("|"))
+    .map((i) => i.item);
