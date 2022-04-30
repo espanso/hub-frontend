47ccc50af1ee20a229ae334f9410f3f6a22ca7bd
matteo-pellegrino <matteo.pelle.pellegrino@gmail.com>
2022-04-30 14:18:08 +0200

    Fix production asset prefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/espanso/hub-frontend/" : "",
+  assetPrefix: isProd ? "/hub-frontend/" : "",
 };
