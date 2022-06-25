3f32d44afb83932bf7e29f78edcfe2b577082560
matteo-pellegrino <matteo.pelle.pellegrino@gmail.com>
2022-06-25 19:00:37 +0200

    feat: Add multi lines split util

--- api/utils.ts
+++ api/utils.ts
@@ -1,5 +1,5 @@
-import { taskEither, either } from "fp-ts";
-import { Either } from "fp-ts/Either";
+import { taskEither, string, array, readonlyNonEmptyArray as rnea } from "fp-ts";
+import { flow, pipe } from "fp-ts/function";
 import { TaskEither } from "fp-ts/TaskEither";
 
 const logMiddleware = <E>(error: E) => {
@@ -10,3 +10,14 @@ const logMiddleware = <E>(error: E) => {
 export const taskEitherLogError: <E, A>(
   taskEither: TaskEither<E, A>
 ) => TaskEither<E, A> = taskEither.mapLeft(logMiddleware);
+
+export const splitLines: (nLines: number) => (s: string) => rnea.ReadonlyNonEmptyArray<string> =
+  (nLines) => (s) => pipe(
+    s,
+    string.split("\n"),
+    rnea.chunksOf(nLines),
+    rnea.map(flow(
+      rnea.intersperse("\n"),
+      rnea.reduce('', (acc, curr) => `${acc}${curr}`))
+    )
+  )
